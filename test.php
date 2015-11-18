09c1cdc725190e889fa91e9bc4612f4087a2f95d
Shunta McDavid <smcdav01@seattlecentral.edu>
2015-11-17 16:32:17 -0800

    Edits test.php
    Our second edit to test.php

--- test.php
+++ test.php
@@ -4,27 +4,15 @@
 
 
 <body>
-<h1>Version 1 of our file</h1>
+<h1>Version 2 of our file</h1>
 <p>Clever content goes here.</p>
-<p>Clever content goes here.</p>
+<p>Different clever content goes here.</p>
+<p>Even more different clever content goes here.</p>
 <p>Clever content goes here.</p>
 <p>Clever content goes here.</p>
 <p>Clever content goes here.</p>
 
 </body>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
